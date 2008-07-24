a519449ac9e401950b35f0e53f00194b15a4776b
Jonathan Stott <jonathan.stott@gmail.com>
2008-07-24 01:33:12 +0100

    Specs like whoa + reimplementation of NoteTaker to go with them.
    Basically all the behaviour the previous implementation has is in the new one
    except for parsing the configuration file, which will now be handled by another
    class, which will probably also take care of the database setup.

--- lib/notable/note_taker.rb
+++ lib/notable/note_taker.rb
@@ -11,26 +11,67 @@ module Notable
       parse_options_hash(options)
     end
 
-    # connects and registers the callback which will save messages
-    def connect
-      @client.connect
-      @client.auth(@pass)
-      @client.send(Jabber::Presence.new.set_type(:available))
-      @client.add_message_callback do |m|
+    # connect to the Jabber server with NoteTaker's credentials
+    def handshake
+      client.connect
+      client.auth(@password)
+      set_available
+    end
+
+    # set the client to available
+    def set_available
+      client.send(Jabber::Presence.new)
+    end
+
+    # register the callbacks for message handling
+    def register_callbacks
+      client.add_message_callback do |m|
         if m.type != :error
-          Note.create(:body => m.body.strip)
-          if @echo
-            reply = Jabber::Message.new(m.from, "You sent: #{m.body}")
-            reply.type = m.type
-            @client.send(reply)
-          end
+          process_message(m)
         end
       end
+    end
+
+    # processes the message, acting on the command it contains, or saving the
+    # message to the database, as appropriate
+    def process_message(message)
+      body = message.body.strip
+      save_message(message)
+      echo_message(message) if echo
+    end
+
+    ##
+    # sends a message via the client.
+    #
+    #
+    def send_message(to, body, type)
+      reply = Jabber::Message.new(to, body)
+      reply.type = type
+      client.send(reply)
+    end
+
+    ##
+    # echos the body of the message back to the sender
+    def echo_message(message)
+      send_message(message.from, "Received: #{message.body}", message.type)
+    end
+
+    ##
+    # Saves the message to the database.
+    def save_message(message)
+      Note.create(:body => message.body.strip)
+    end
+
+    # connects and registers the callback which will save messages
+    def connect
+      handshake
+      register_callbacks
     end
 
     protected
     def parse_options_hash(options)
       @echo = options.delete('echo') if options.has_key?('echo')
     end
+
   end
 end
