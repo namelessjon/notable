1efc55df3ba9b6422571a7f2713a6300247619bd
Jonathan Stott <jonathan.stott@gmail.com>
2008-08-23 15:35:16 +0100

    Added simple search command.

--- lib/notable/note_taker.rb
+++ lib/notable/note_taker.rb
@@ -2,13 +2,26 @@
 module Notable
   class NoteTaker < ::Uppercut::Agent
 
-    command /^last ?(\d+)?$/ do |c, rest|
+    command(/^last ?(\d+)?$/) do |c, rest|
       last = rest.to_i unless rest.nil?
       notes = Note.all(:limit => (last||5), :order => [:created_at.desc])
       c.send(notes.map {|n| n.body }.join("\n"))
     end
 
-    command /^(.*)$/ do |c, rest|
+    command(/^search ?(\w+)?$/) do |c, term|
+      if term.nil?
+        c.send("Send a search term. e.g 'search todo'")
+      else
+        notes = Note.all(:body.like => "%#{term}%")
+        string = <<-eos
+        Results for #{term}:
+        #{notes.map {|n| n.body }.join("\n")}
+        eos
+        c.send(string)
+      end
+    end
+
+    command(/^(.*)$/) do |c, rest|
       Note.create(:body => rest.strip)
     end
   end
