a34cbc4955ba052703f8e993333a5b72dbd6faa3
Jonathan Stott <jonathan.stott@gmail.com>
2008-12-07 20:01:57 +0000

    note_taker (jabber client) is now a standalone daemon
    * interacts with the main app via HTTP interface
    * takes simple config file
    * runs as a daemon

--- lib/notable/note_taker.rb
+++ lib/notable/note_taker.rb
@@ -1,28 +1,69 @@
+begin
+  require 'minigems'
+rescue LoadError
+  require 'rubygems'
+end
 
-module Notable
-  class NoteTaker < ::Uppercut::Agent
+gem('tyler-uppercut')
+require 'uppercut'
 
-    command(/^last ?(\d+)?$/) do |c, rest|
-      last = rest.to_i unless rest.nil?
-      notes = Note.all(:limit => (last||5), :order => [:created_at.desc])
-      c.send(notes.map {|n| n.body }.join("\n"))
-    end
+gem('json')
+require 'json/ext'
 
-    command(/^search ?(\w+)?$/) do |c, term|
-      if term.nil?
-        c.send("Send a search term. e.g 'search todo'")
-      else
-        notes = Note.all(:body.like => "%#{term}%")
-        string = <<-eos
+gem('rest-client')
+require 'rest_client'
+
+require 'daemons'
+
+class NoteTaker < ::Uppercut::Agent
+  def initialize(opts)
+    setup_resource(opts)
+    super(opts['jabber_username'], opts['jabber_password'], :connect => false)
+  end
+
+
+  command(/^last ?(\d+)?$/) do |c, rest|
+    last = rest.to_i unless rest.nil?
+    notes = get("/last/#{last}")
+    c.send(notes.map {|n| n['body'] }.join("\n"))
+  end
+
+  command(/^search ?(\w+)?$/) do |c, term|
+    if term.nil?
+      c.send("Send a search term. e.g 'search todo'")
+    else
+      notes = get("/search?q=#{term}")
+      string = <<-eos
         Results for #{term}:
-        #{notes.map {|n| n.body }.join("\n")}
+      #{notes.map {|n| n['body'] }.join("\n")}
         eos
-        c.send(string)
-      end
+      c.send(string)
     end
+  end
 
-    command(/^(.*)$/m) do |c, rest|
-      Note.create(:body => rest.strip)
+  command(/^(.*)$/m) do |c, rest|
+    begin
+      c.send(r.post(:note => rest))
+    rescue RestClient::RequestFailed => e
+      c.send("Failed: #{e.response.body}")
     end
+  end
+
+  protected
+  def setup_resource(opts)
+    if (opts['http_username']&&opts['http_password'])
+      @@resource = ::RestClient::Resource.new(opts['url'],
+                                   opts['http_username'], opts['http_password'])
+    else
+      @@resource = ::RestClient::Resource.new(opts['url'])
+    end
+  end
+
+  def self.r
+    @@resource
+  end
+
+  def self.get(url, opts={})
+    ::JSON.parse(r[url].get(opts.merge({:accept => 'application/json'})))
   end
 end
